e74e5980cfd0fa81961f26423d45519253e0892a
William Goins <de777urest@gmail.com>
2017-01-16 18:25:00 -0600

    Added solution to Loops tutorial

---
+++ src/hackerrank/Loops.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+/**
+ *
+ * @author willi
+ */
+public class Loops {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + n * i);
+        }
+    }
+    
+}
