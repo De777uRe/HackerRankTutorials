fcf8277e73d800849f8943d5ddbc82a542ab3828
William Goins <de777urest@gmail.com>
2017-01-19 00:08:20 -0600

    Added Arrays Tutorial Solution

---
+++ src/hackerrank/Arrays.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author willi
+ */
+public class Arrays {
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        in.close();
+        for(int i = n-1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
