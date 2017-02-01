3b580b89b67652d7bb9cde7ee2ccdb46dec3275c
William Goins <de777urest@gmail.com>
2017-01-31 23:06:04 -0600

    Added Solution to BubbleSort Tutorial

--- src/hackerrank/ArraysTutorial.java
+++ src/hackerrank/ArraysTutorial.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *
  * @author willi
  */
-public class Arrays {
+public class ArraysTutorial {
     
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
